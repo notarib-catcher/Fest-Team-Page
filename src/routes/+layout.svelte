89359c5daa39bcef4dbb71c63e4e39610935b38b
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-09-05 02:10:13 +0530

    added SURGE logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../app.css";
+    import SURGE from "../assets/SURGE-noFLKTXT.webp"
 </script>
 
 <slot />
+<div class="fixed bottom-0 right-0 z-0 pointer-events-none">
+    <img src={SURGE} class="mr-2 mb-2 opacity-50" width="80px">
+</div>

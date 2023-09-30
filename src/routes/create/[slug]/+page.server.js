3a2141a91108971f8fbf2eebbea3d0647edbaf2e
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-10-01 01:39:10 +0530

    Updates to event structure

--- src/routes/create/[slug]/+page.server.js
+++ src/routes/create/[slug]/+page.server.js
@@ -18,60 +18,85 @@ const passes_db = dbclient.db("ticketing")
 const passes = passes_db.collection("passes")
 
 const eventsToPasses = {
-    "S_FB_M" : ["SPORT_FB_M"], //Team event
-    "S_BB_M" : ["SPORT_BB_M"], //Team event
-    "S_VB_M" : ["SPORT_VB_M"], //Team event
-    "S_TN_M" : ["SPORT_TN_M"], //Team event
-    "S_TT_M" : ["SPORT_TT_M"], //Team event
-    "S_BB_F" : ["SPORT_BB_F"], //Team event
-    "S_TB_F" : ["SPORT_TB_F"], //Team event
-    "S_TN_F" : ["SPORT_TN_F"], //Team event
-    "S_TT_F" : ["SPORT_TT_F"], //Team event
-    "S_CHS" : ["SPORT_CHS"], //Team event
-    "C_BOB" : ["CLTR_BOB"], //Team event
-    "C_GRD" : ["CLTR_GRD"], //Team event
-    "C_FAS" : ["CLTR_FAS"],
-    "C_STRSPL" : ["CLTR_PRO"],
-    "C_THESHD" : ["CLTR_PRO"],
-    "C_MNDSCR" : ["CLTR_PRO"],
-    "C_MKTMHM" : ["CLTR_PRO"],
-    "C_RANCOM" : ["CLTR_PRO"],
-    "C_BTBLBD" : ["CLTR_PRO"],
-    "C_NATNIR" : ["CLTR_PRO"],
-    "C_FCSFRM" : ["CLTR_PRO"],
-    "C_RMXRYL" : ["CLTR_PRO"],
-    "C_MNGODY" : ["CLTR_PRO"],
-    "C_BSHBTL" : ["CLTR_PRO"],
-    "C_QZAPLZ" : ["CLTR_PRO"],
-    "C_ZENITH" : ["CLTR_PRO"],
-    "C_STRBET" : ["CLTR_PRO"],
-    "C_BRNWAV" : ["CLTR_PRO"],
-    "C_SAARANG" : ["CLTR_PRO"],
-    "ES_BGDMHM" : ["ESPORTS"],
-    "ES_VALRIS" : ["ESPORTS"],
-    "ES_CODEXE" : ["ESPORTS"],
-    "ES_CRWQST" : ["ESPORTS"],
-    "ES_GOLQST" : ["ESPORTS"]
 
+    //Sports (only team events are mentioned here)
+    "S_FB_M" : ["SPORT_FB_M"],
+    "S_BB_M" : ["SPORT_BB_M"],
+    "S_VB_M" : ["SPORT_VB_M"],
+    "S_TN_M" : ["SPORT_TN_M"],
+    "S_TT_M" : ["SPORT_TT_M"],
+    "S_BB_F" : ["SPORT_BB_F"],
+    "S_TB_F" : ["SPORT_TB_F"],
+    "S_CHS" : ["SPORT_CHS"],
 
+    //Cultural Featured
+    // "C_PRO" : ["CLTR_PRO", "SUP_PRO"], //no regs
+    "C_BOB" : ["CLTR_BOB"], //team, 8
+    "C_GRC" : ["CLTR_GRD"], //team, 15
+    "C_GRW" : ["CLTR_GRD"], //team, 20
+    "C_FAS" : ["CLTR_FAS"], //team, 10
 
+    //Cultural Normie
+    // "C_MNDSCR": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_BTSCAR": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_STRBET": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_BTBLBD": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    // "C_FCSFRM": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_ZENITH": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_SHKTNK": ["CLTR_PRO", "SUP_PRO"], //team, 7
+    // "C_BSHBTL": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_MNGODY": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_RANCOM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    "C_THESHD": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    "C_STRSPL": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    "C_MKTMHM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    "C_CINCRF": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    "C_BTLBAB": ["CLTR_PRO", "SUP_PRO"], //team, 2
+
+    //Esports
+    "ES_BGDMHM" : ["ESPORTS"], //team, 4
+    "ES_VALRIS" : ["ESPORTS"], //team, 7
+    "ES_CODEXE" : ["ESPORTS"], //team, 5
+    "ES_CRWQST" : ["ESPORTS"], //team, 2
+    // "ES_GOLQST" : ["ESPORTS"]  //solo
 }
 
 const maxmems = {
     //sport team events
-    "S_TN_M" : 4,
-    "S_TN_F" : 4,
-    "S_TT_M" : 4,
-    "S_TT_F" : 4,
+
     "S_FB_M" : 18,
+    "S_BB_M" : 12,
+    "S_TN_M" : 4,
+    "S_TT_M" : 4,
     "S_VB_M" : 12,
     "S_TB_F" : 12,
-    "S_BB_M" : 12,
     "S_BB_F" : 12,
     "S_CHS" : 6,
-    //cultural events
+
+    //cultural (featured) events
+
     "C_BOB" : 8,
-    "C_GRD" : 10,
+    "C_GRC" : 15,
+    "C_GRW" : 20,
+    "C_FAS" : 10,
+
+    //cultural (regular) events
+
+    "C_BTBLBD": 4,
+    "C_SHKTNK": 7,
+    "C_RANCOM": 3,
+    "C_THESHD": 20,
+    "C_STRSPL": 20,
+    "C_MKTMHM": 3,
+    "C_CINCRF": 4,
+    "C_BTLBAB": 2,
+
+    //esports events
+
+    "ES_BGDMHM": 4,
+    "ES_VALRIS": 7,
+    "ES_CODEXE": 5,
+    "ES_CRWQST": 2
 
 
 
@@ -102,8 +127,9 @@ export const load =  async (/** @type {{ locals: { getSession: () => any; }; }} 
      * - Create the team
      */
 
-    let eventDoc = await t_events.findOne({_id:slug})
-    if(!eventDoc || eventDoc?.solo){
+    let eventDoc = await t_events.findOne({_id:slug, solo:false, needreg: true})
+
+    if(!eventDoc){
         throw redirect(302, "https://falak.mitblrfest.in/events/" + slug)
     }
 
--- src/routes/regevent/[slug]/+page.server.js
+++ src/routes/regevent/[slug]/+page.server.js
@@ -22,43 +22,41 @@ const passes_db = dbclient.db("ticketing")
 const passes = passes_db.collection("passes")
 
 const eventsToPasses = {
-    //"S_FB_M" : ["SPORT_FB_M"], //Team event
-    //"S_BB_M" : ["SPORT_BB_M"], //Team event
-    //"S_VB_M" : ["SPORT_VB_M"], //Team event
-    //"S_TN_M" : ["SPORT_TN_M"], //Team event
-    //"S_TT_M" : ["SPORT_TT_M"], //Team event
-    //"S_BB_F" : ["SPORT_BB_F"], //Team event
-    //"S_TB_F" : ["SPORT_TB_F"], //Team event
-    // "S_TN_F" : ["SPORT_TN_F"],
-    // "S_TT_F" : ["SPORT_TT_F"],
-    "S_ATH" : ["SPORT_ATH"],
-    // "S_CHS" : ["SPORT_CHS"],
-    //"C_PRO" : ["CLTR_PRO"], //No need to register
-    //"C_BOB" : ["CLTR_BOB"], //Team event
-    //"C_GRD" : ["CLTR_GRD"], //Team event
-    "C_FAS" : ["CLTR_FAS"],
-    "C_STRSPL" : ["CLTR_PRO"],
-    "C_THESHD" : ["CLTR_PRO"],
-    "C_MNDSCR" : ["CLTR_PRO"],
-    "C_MKTMHM" : ["CLTR_PRO"],
-    "C_RANCOM" : ["CLTR_PRO"],
-    "C_BTBLBD" : ["CLTR_PRO"],
-    "C_NATNIR" : ["CLTR_PRO"],
-    "C_FCSFRM" : ["CLTR_PRO"],
-    "C_RMXRYL" : ["CLTR_PRO"],
-    "C_MNGODY" : ["CLTR_PRO"],
-    "C_BSHBTL" : ["CLTR_PRO"],
-    "C_QZAPLZ" : ["CLTR_PRO"],
-    "C_ZENITH" : ["CLTR_PRO"],
-    "C_STRBET" : ["CLTR_PRO"],
-    "C_BRNWAV" : ["CLTR_PRO"],
-    "C_SAARANG" : ["CLTR_PRO"],
-    "ES_BGDMHM" : ["ESPORTS"],
-    "ES_VALRIS" : ["ESPORTS"],
-    "ES_CODEXE" : ["ESPORTS"],
-    "ES_CRWQST" : ["ESPORTS"],
-    "ES_GOLQST" : ["ESPORTS"]
 
+    "S_TN_F" : ["SPORT_TN_F"],
+    "S_TT_F" : ["SPORT_TT_F"],
+    "S_ATH" : ["SPORT_ATH"],
+
+    //Cultural Featured
+    // "C_PRO" : ["CLTR_PRO", "SUP_PRO"], //no regs
+    // "C_BOB" : ["CLTR_BOB"], //team, 8
+    // "C_GRC" : ["CLTR_GRD"], //team, 15
+    // "C_GRW" : ["CLTR_GRD"], //team, 20
+    // "C_FAS" : ["CLTR_FAS"], //team, 10
+
+    //Cultural normie
+    "C_MNDSCR": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_BTSCAR": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_STRBET": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_BTBLBD": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    "C_FCSFRM": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_ZENITH": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_SHKTNK": ["CLTR_PRO", "SUP_PRO"], //team, 7
+    "C_BSHBTL": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_MNGODY": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_RANCOM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    // "C_THESHD": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    // "C_STRSPL": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    // "C_MKTMHM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    // "C_CINCRF": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    // "C_BTLBAB": ["CLTR_PRO", "SUP_PRO"], //team, 2
+
+    //Esports
+    // "ES_BGDMHM" : ["ESPORTS"], //team, 4
+    // "ES_VALRIS" : ["ESPORTS"], //team, 7
+    // "ES_CODEXE" : ["ESPORTS"], //team, 5
+    // "ES_CRWQST" : ["ESPORTS"], //team, 2
+    "ES_GOLQST" : ["ESPORTS"]  //solo
 
 
 }
@@ -131,7 +129,7 @@ const checkIfAllowedToRegister = async (email, event) => {
     * - Register the user for that event
     * - Redirect them to the event page
     */
-    const eventDoc = await t_events.findOne({_id:event, solo:true})
+    const eventDoc = await t_events.findOne({_id:event, solo:true, needreg: true})
 
     if(!eventDoc){
         console.log(1)

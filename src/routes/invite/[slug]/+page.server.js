51093225061f9350b4c9bc1264125867a86fc0ed
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-10-04 23:47:52 +0530

    Force team members to buy passes for regular cultural events

--- src/routes/invite/[slug]/+page.server.js
+++ src/routes/invite/[slug]/+page.server.js
@@ -17,6 +17,58 @@ const t_users = database.collection(process.env.DB_COLL_PREFIX + "users")
 const t_events = database.collection(process.env.DB_COLL_PREFIX + "events")
 /*end standard boilerplate*/
 
+const passes_db = dbclient.db("ticketing")
+const passes = passes_db.collection("passes")
+
+const eventsToPasses = {
+
+    //Sports (only team events are mentioned here)
+    "S_FB_M" : ["SPORT_FB_M"],
+    "S_BB_M" : ["SPORT_BB_M"],
+    "S_VB_M" : ["SPORT_VB_M"],
+    "S_TN_M" : ["SPORT_TN_M"],
+    "S_TT_M" : ["SPORT_TT_M"],
+    "S_BB_F" : ["SPORT_BB_F"],
+    "S_TB_F" : ["SPORT_TB_F"],
+    "S_CHS" : ["SPORT_CHS"],
+
+    //Cultural Featured
+    // "C_PRO" : ["CLTR_PRO", "SUP_PRO"], //no regs
+    "C_BOB" : ["CLTR_BOB"], //team, 8
+    "C_GRC" : ["CLTR_GRD"], //team, 15
+    "C_GRW" : ["CLTR_GRD"], //team, 20
+    "C_FAS" : ["CLTR_FAS"], //team, 10
+
+    //Cultural Normie
+    // "C_MNDSCR": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_BTSCAR": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_STRBET": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_BTBLBD": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    // "C_FCSFRM": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_ZENITH": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_SHKTNK": ["CLTR_PRO", "SUP_PRO"], //team, 7
+    // "C_BSHBTL": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_MNGODY": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_RANCOM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    "C_THESHD": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    "C_STRSPL": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    "C_MKTMHM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    "C_CINCRF": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    "C_BTLBAB": ["CLTR_PRO", "SUP_PRO"], //team, 2
+    // "C_ECHOES": ["CLTR_PRO", "SUP_PRO"], //solo
+    // "C_LOGLYM": ["CLTR_PRO", "SUP_PRO"], //solo REMOVED
+    "C_SRGSAF": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    "C_BRNWAV": ["CLTR_PRO", "SUP_PRO"],
+    "C_QZAPLZ": ["CLTR_PRO", "SUP_PRO"],
+
+    //Esports - ALL WERE MADE SOLO
+    // "ES_BGDMHM" : ["ESPORTS"], //team, 4
+    // "ES_VALRIS" : ["ESPORTS"], //team, 7
+    // "ES_CODEXE" : ["ESPORTS"], //team, 5
+    // "ES_CRWQST" : ["ESPORTS"], //team, 2
+    // "ES_GOLQST" : ["ESPORTS"]  //solo
+}
+
 export const load =  async (/** @type {{ locals: { getSession: () => any; }; }} */ event) => {
 
     const session = await event.locals.getSession();
@@ -61,7 +113,28 @@ export const load =  async (/** @type {{ locals: { getSession: () => any; }; }} 
         }
     }
 
-    //No team with same event on this account - go ahead and add
+    //No team with same event on this account - check if eligible
+
+
+    if(cteam.event.startsWith("C_") && cteam.event.length >= 6){
+
+        //pass is needed
+
+        let allowed = false
+        for(let passtype of (eventsToPasses[cteam.event] || ["NEXIST"])){
+            const passDoc = await passes.findOne({email:session.user.email, type:passtype})
+            if(passDoc){
+                allowed = true
+                break
+            }
+        }
+
+        if(!allowed){
+            throw redirect(302, "https://falak.mitblrfest.in/events/" + cteam.event)
+        }
+
+    }
+
     const ID = cteam._id.toString()
     delete cteam._id
     cteam.members.push({name: curr_user.name, email: session.user.email})
